168a459c1ef024ce6f2de3810e29248d3057a4dc
Lohitaksh Trehan <lohitakshtrehan@gmail.com>
2017-12-21 21:45:27 +0530

    first vue website
    Signed-off-by: Lohitaksh Trehan <lohitakshtrehan@gmail.com>

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>{{copyright}}</p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  data:function() {
+    return {
+      copyright: 'copyright 2018 vue ninjas'
+    };
+  }
+}
+
+</script>
+
+<style scoped>
+  footer{
+    background: #222;
+    padding: 6px;
+  }
+  p{
+    color: lightgreen;
+    text-align: center;
+  }
+</style>
